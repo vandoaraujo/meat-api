22c7f0a80a6ffdd008da6d931fd5a91a60f20870
Vando Araujo <tecladista.vando@gmail.com>
2019-11-17 22:54:11 -0300

    cadeia de metodos node

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # meat-api
 api node js
+
+Restify era 5.0.8
--- dist/main.js
+++ dist/main.js
@@ -5,10 +5,23 @@ const server = restify.createServer({
     name: 'meat-api',
     version: '1.0.0'
 });
-server.get('/hello', (req, resp, next) => {
-    resp.json({ message: 'hello' });
-    return next();
-});
+server.use(restify.plugins.queryParser());
+server.get('/info', [
+    (req, resp, next) => {
+        return next();
+    }, (req, resp, next) => {
+        //resp.json({message: 'hello'})
+        //return next()
+        resp.json({
+            browser: req.userAgent(),
+            method: req.method,
+            url: req.href(),
+            path: req.path(),
+            query: req.query
+        });
+        return next();
+    }
+]);
 server.listen(3000, () => {
     console.log('API is running on https://localhost:3000');
 });
