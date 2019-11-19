e9103ad6164018661e1d0ea22c4bdd7f9f6d1864
Vando Ribeiro de Araujo <tecladista.vando@gmail.com>
2019-11-19 15:38:25 -0300

    adicao erros especificos

--- main.ts
+++ main.ts
@@ -9,6 +9,15 @@ server.use(restify.plugins.queryParser())
 
 server.get('/info', [
     (req, resp, next) =>{
+        if(req.userAgent() && req.userAgent().includes('MSIE 7.0')){
+            //resp.status(400)
+            //resp.json({message: 'Please update your browser!'})
+            //return next(false)
+            let error: any = new Error()
+            error.statusCode = 400
+            error.message = 'Please update your browser!'
+            return next(error)
+        } 
         return next()
 },  (req, resp, next) =>{
     //resp.json({message: 'hello'})
